26de0156a20d57877fff98a34448014c3dc7f9a8
Local_clone <mainkham4@gmail.com>
2024-06-05 08:32:27 +0630

    Making some test

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
 const express = require('express')
 const app = express()
+const session = require('express-session')
+const BodyParser = require('body-parser')
 app.set("view engine","ejs")
+app.use(BodyParser.urlencoded({extended:true}))
 
 
+app.use(session({
+    secret:"NoOneKnow",
+    resave:false,
+    saveUninitialized:false,
+}))
 
-
-app.get('/',(req,res) =>{
+app.get('/home',(req,res) =>{
     res.render('home')
+    
+    
 })
 
+
 app.get('/register.ejs',(req,res) =>{
+
     res.render('register')
+    if(req.session){
+        console.log(req.session.user)
+    }
+
+
+   
+    
+    
+    
+})
+app.get('/test',(req,res) =>{
+    if(!req.session.user){
+        req.session.user = {
+            name:"Guest",
+            role:"Visitor"
+        }
+    }
+    else
+    {console.log("Old-user")}
+
+    }
+    
+    
+)
+
+app.post('/register.ejs',(req,res) =>{
+    console.log(`User_name:${req.body.name} and Password is ${req.body.pass}`)
+    res.redirect('/')
 })
 
 
